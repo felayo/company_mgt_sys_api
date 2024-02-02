0010e284204a93517993ef5f04b887d2ea1ec2e0
Ayorinde <ajibaye@gmail.com>
2024-02-02 13:35:59 +0100

    cors allowed origin updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://www.winelightanalytical.com/portal',
     'http://127.0.0.1:5173',
+    'https://winelight-portal.netlify.app',
 ]
 
 module.exports = allowedOrigins
